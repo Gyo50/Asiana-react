c3e7793ef1285b05c452d7276b258d6c0a64f93f
오영교 <oey157@nate.com>
2025-02-22 21:22:50 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './header/header';
-import Test from './test/test';
 
 function App() {
   return (
-  // <Header/>
-    <Test />
+  <Header/>
+
   );
 }
 
